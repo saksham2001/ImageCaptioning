c8a3d44e7b8644f0a14a6df29687f3fb16edef5f
Saksham Bhutani <sakshambhutani2001@gmail.com>
2020-07-22 18:04:45 +0530

    Reduced Dependencies

--- LSTM.py
+++ LSTM.py
@@ -1,10 +1,10 @@
 import re
 import collections
+import tensorflow as tf
 from tensorflow.keras.models import load_model, Model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-import numpy as np
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 
 n = 0
 max_len = 38
@@ -119,7 +119,9 @@ class LSTM:
     def preprocess_img(self, img):
         img = image.load_img(img, target_size=(224, 224))
         img = image.img_to_array(img)
-        img = np.expand_dims(img, axis=0)
+        print(img.shape)
+        img = tf.expand_dims(img, axis=0)
+        print(img.shape)
         # Normalisation
         img = preprocess_input(img)
         return img
